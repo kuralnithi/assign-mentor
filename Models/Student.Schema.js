e21554336ebce4f92379a908b86a8346e8f16afc
kuranithi <kuralnithi1999@gmail.com>
2023-11-08 13:29:57 +0530

    aa

--- Models/Student.Schema.js
+++ Models/Student.Schema.js
@@ -3,12 +3,9 @@ import mongoose from 'mongoose';
 const StudentSchema = mongoose.Schema({
   StudentName: String,
   Subject: String,
-  Mentor: {
-    type: mongoose.Schema.Types.ObjectId
-  }
+  Mentor: mongoose.Schema.Types.ObjectId
 });
 
 const StudentModel = mongoose.model('StudentModel', StudentSchema);
 
 export default StudentModel;
- 
