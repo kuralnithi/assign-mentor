eefccc59224d48f03ddfa4ceb0cf7053b63a01c6
kuranithi <kuralnithi1999@gmail.com>
2023-11-08 12:58:38 +0530

    zz

--- Controllers/Mentor.Controller.js
+++ Controllers/Mentor.Controller.js
@@ -68,11 +68,11 @@ export const getStudentsForMentor = async (req, res) => {
         as: "mentorDetails",
       },
     },
-    // {
-    //   $match: {
-    //     mentorDetails: { $ne: [] }, // This filters out documents without a matching mentor in the foreign collection
-    //   },
-    // },
+    {
+      $match: {
+        mentorDetails: { $ne: [] }, // This filters out documents without a matching mentor in the foreign collection
+      },
+    },
   ]);
 
     console.log("students",students);
